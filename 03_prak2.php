2958a3eb91f89e7335fd1ac7525b729360ffb5e1
muliansyahr21 <muliansyah.r@students.amikom.ac.id>
2018-12-08 13:43:22 +0700

    Create 03_prak2.php

---
+++ 03_prak2.php
@@ -0,0 +1,17 @@
+<?php
+$nilai = 80;
+if(($nilai >= 85) && ($nilai <= 100)){
+$index = "A";
+} else if($nilai >= 70) {
+$index = "B";
+} else if($nilai >= 50) {
+$index = "C";
+} else if($nilai >= 30){
+$index = "D";
+} else {
+$index = "E";
+}
+echo "Nilai anda adalah : ".$nilai;
+echo "<br />";
+echo "Dengan index : ".$index;
+?>
